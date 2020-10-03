e9c5ddbbfbeff052de54fdec6743a0412efd11c6
Daniel <danisan9490@gmail.com>
2020-10-03 17:09:38 +0100

    #9 Add Form Validation

--- insurance-quote/src/Components/Formulario.js
+++ insurance-quote/src/Components/Formulario.js
@@ -36,14 +36,25 @@ const Boton = styled.button`
     }
 `;
 
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width:100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
   const [datos, guardarDatos] = useState({
     marca: '',
     year: '',
     plan: ''
   });
+  const [error, guardarError] = useState(false);
 
   const { marca, year, plan } = datos;
+
 
   const obtenerInformacion = e => {
     guardarDatos({
@@ -51,8 +62,38 @@ const Formulario = () => {
       [e.target.name]: e.target.value
     })
   }
+  const cotizarSeguro = e => {
+    e.preventDefault();
+
+    if (marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    // obtener la diferencia de años
+
+    // por cada año hay que restar el 3%
+
+    // Americano 15
+    // Asiatico 5%
+    // Europeo 30%
+
+
+    // Basíco aumenta 20%
+    // Completo 50%
+
+
+    // Total
+
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={cotizarSeguro}
+    >
+      { error ? <Error>Todos los campos son obligatorios</Error> : null}
       <Campo>
         <Label>Brand</Label>
         <Select
@@ -106,7 +147,7 @@ const Formulario = () => {
           onChange={obtenerInformacion}
         /> Complete
         </Campo>
-      <Boton type="button">Calculate</Boton>
+      <Boton type="submit">Calculate</Boton>
     </form>
 
   );
