badd96a1646271605d2e2ef459acd8517e62df59
Daniel <danisan9490@gmail.com>
2020-10-03 17:47:59 +0100

    Updates

---
+++ precache-manifest.fd60e03e506b0e1d54a7621042bf3367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f1a617b908b44c85b2ab5c8131492b",
+    "url": "/insurance-calculator/index.html"
+  },
+  {
+    "revision": "98bbc25c748ca2f2cdda",
+    "url": "/insurance-calculator/static/css/main.360d6756.chunk.css"
+  },
+  {
+    "revision": "d5212b869d70e5573ca8",
+    "url": "/insurance-calculator/static/js/2.29f90ad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/insurance-calculator/static/js/2.29f90ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98bbc25c748ca2f2cdda",
+    "url": "/insurance-calculator/static/js/main.ca9fe5c7.chunk.js"
+  },
+  {
+    "revision": "c048cd5bddd6ebe3b68e",
+    "url": "/insurance-calculator/static/js/runtime-main.a6894218.js"
+  }
+]);
